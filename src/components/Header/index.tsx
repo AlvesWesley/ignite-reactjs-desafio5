bb399e933acddc36c616da54e06fdfbc513296c0
Wesley Alves <wesley_alv@outlook.com>
2021-09-18 10:13:07 -0300

    adicionando estilos globais e header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a href="/">
+          <img src="/images/logo.svg" alt="spacetraveling" />
+        </a>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,40 @@
-import { GetStaticProps } from 'next';
+// import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
-import { getPrismicClient } from '../services/prismic';
+// import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+// import commonStyles from '../styles/common.module.scss';
+// import styles from './home.module.scss';
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
-}
-
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
+// interface Post {
+//   uid?: string;
+//   first_publication_date: string | null;
+//   data: {
+//     title: string;
+//     subtitle: string;
+//     author: string;
+//   };
 // }
+
+// interface PostPagination {
+//   next_page: string;
+//   results: Post[];
+// }
+
+// interface HomeProps {
+//   postsPagination: PostPagination;
+// }
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Início | spacetraveling</title>
+      </Head>
+      <h1>Home</h1>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
